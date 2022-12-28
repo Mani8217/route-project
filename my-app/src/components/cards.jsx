b00fb5575448a140f0a947dbcb3e7783be45f7d1
mani <100167532+Mani8217@users.noreply.github.com>
2022-12-29 00:55:59 +0100

    badge correction

--- my-app/src/components/cards.jsx
+++ my-app/src/components/cards.jsx
@@ -1,14 +1,14 @@
 
 const Cards = (props) => {
      let badgeText;
-     if (props.openSpot === 0 ){
+     if (props.openspots === 0 ){
         badgeText = 'Sold Out';
      }else {
-        badgeText = 'reserve'
+        badgeText = 'Reserve';
      }
     return (
         <div className="card">
-
+         {badgeText && <div class="badge bg-danger">{badgeText}</div>} 
          <img src={props.img} className="card-img-top" alt="..." />
          <div className="card-body">
          <h5 className="card-title">{props.title}</h5>
